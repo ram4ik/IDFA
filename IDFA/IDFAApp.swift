17555224c513df277263cbdf3fcefb1f40d2c6f0
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-04-20 09:01:16 +0300

    Initial Commit

---
+++ IDFA/IDFAApp.swift
@@ -0,0 +1,17 @@
+//
+//  IDFAApp.swift
+//  IDFA
+//
+//  Created by ramil on 20.04.2021.
+//
+
+import SwiftUI
+
+@main
+struct IDFAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
